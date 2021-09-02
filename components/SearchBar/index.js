209e43baf3a05581cc4a41ee4462378aaf95e87e
Vinicius Batista <vinicius-moura-batista@hotmail.com>
2021-09-02 18:48:35 -0300

    first commit

---
+++ components/SearchBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Search.module.scss';
+
+function SearchBar({...rest}) {
+    return (
+        <div className={styles.coin_search}> 
+            <input className={styles.coin_input} {...rest} />
+        </div>
+    )
+}
+
+export default SearchBar
